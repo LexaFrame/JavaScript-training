679d5b3fe10b3fd8e6d29d27e7691281bb033e22
LexaFrame <sarah.bilger@free.fr>
2025-11-30 04:00:00 +0100

    exercice 8 terminé

--- exo08.js
+++ exo08.js
@@ -2,47 +2,43 @@ import readlineSync from 'readline-sync';
 const nom_magasin = readlineSync.question('Quel est le nom du magasin ? ');
 
 let prix_jeu = 50;
-let reduction_ete = 0.2;
-let reduction_hiver = 0.3;
+let reduction_ete = 20;
+let reduction_hiver = 30;
+let reduction_autre = 15;
 let prix_final_ete = 0;
-let prix_final_hiver =0;
+let prix_final_hiver = 0;
+let prix_final_autre = 0;
 
 function promotion_ete(prix_jeu, reduction_ete) { //Promotion : -20%
-    return (prix_jeu * reduction_ete/100)+prix_jeu
+    return prix_jeu-(prix_jeu * reduction_ete/100)
 }
 prix_final_ete = promotion_ete(prix_jeu, reduction_ete);
 
 function promotion_hiver(prix_jeu, reduction_hiver) { //Promotion : -30%
-    return (prix_jeu * reduction_hiver/100)+prix_jeu
+    return prix_jeu-(prix_jeu * reduction_hiver/100)
 }
 prix_final_hiver = promotion_hiver(prix_jeu, reduction_hiver);
 
-const ete = prix_final_ete
-const hiver = prix_final_hiver
+function promotion_autre(prix_jeu, reduction_autre) { //Promotion : -15%
+    return prix_jeu-(prix_jeu * reduction_autre/100)
+}
+prix_final_autre = promotion_autre(prix_jeu, reduction_autre);
 
-const choix_promo = readlineSync.question('Saisir "ete" ou "hiver" pour connaitre le prix du jeu selon la promotion souhaitee : ')
-// let codes_secrets = "Delta";
+const phrase1 = `Les jeux de ${nom_magasin} sont habituellement vendus au prix de ${prix_jeu} € , mais ${nom_magasin} fait parfois des promotions.`
+console.log(phrase1)
+
+let choix_promo = readlineSync.question(`Saisir "ete" ou "hiver" ou "autre" pour connaitre le prix du jeu selon la promotion souhaitee : `)
 
 switch (choix_promo) {
     case "ete":
-        console.log("Lancer la promotion de la semaine");
+        console.log(`Prix du jeu après application de la promotion d'été : ${prix_final_ete} €.`);
         break;
-    case "Bravo":
-        console.log("Réorganiser la section des jeux rétro");
+    case "hiver":
+        console.log(`Prix du jeu après application de la promotion d'hiver : ${prix_final_hiver} €.`);
         break;
-    case "Charlie":
-        console.log("Préparer la zone de démonstration pour les nouveaux arrivages");
-        break;
-    case "Delta":
-        console.log("Vérifier l'inventaire des accessoires");
+    case "autre":
+        console.log(`Prix du jeu après application de la promotion spéciale : ${prix_final_autre} €.`);
         break;
     default:
-        console.log("Code inconnu");
+        console.log(`Le prix d'origine du jeu est : ${prix_jeu} €.`);
 }
-
-const phrase1 = `Les jeux de ${nom_magasin} sont habituellement vendus au prix de ${prix_jeu}, mais ${nom_magasin} fait parfois des promotions.`
-console.log(phrase1)
-readlineSync.question('Saisir "ete" ou "hiver" pour connaitre le prix du jeu selon la promotion souhaitee : ')
-const phrase3 = `Prix du jeu apres application de la promotion souhaitee : ${prix_final} €.`;
-console.log(phrase3);
-//if ou switch : si l'utilisateur saisit ete, afficher promotion_ete, si l'utilisateur saisit hiver, afficher promotion_hiver
