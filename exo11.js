502632686908d3bd1f0a8f111b558dfa35c382ec
LexaFrame <sarah.bilger@free.fr>
2025-12-02 11:11:17 +0100

    exercice11

--- exo11.js
+++ exo11.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-const nom_magasin = readlineSync.question('Quel est le nom du magasin ? ');
 
 let jeu1 = {
     titre: "Shin Megami Tensei Persona 3",
@@ -9,31 +8,36 @@ let jeu1 = {
     scorePopularite: 0,
 
     afficher() {
-        console.log(`Le jeu ${this.titre}, ${this.genre} sorti en ${this.annee_de_sortie}, a un score de ${this.score}.`);
-    },
-// Voir pourquoi la méthode ne fonctionne pas 
+        console.log(`Le jeu ${this.titre}, ${this.genre} sorti en ${this.annee_de_sortie}, a un score général de ${this.score}.`);
+    }, 
     getPopularite() {
         this.scorePopularite = (this.score*this.annee_de_sortie/100);
         return this.scorePopularite;
     }
 }
-jeu1.afficher();
-console.log(`Le score de popularité de ${jeu1.titre} est : ${jeu1.getPopularite()}`);
+console.log(`Le score de popularité de ${jeu1.titre} est : ${jeu1.getPopularite().toFixed(2)}`);
 
 let jeu2 = {
     titre: "Ragnarok Online",
     annee_de_sortie: 2007,
     genre: "MMORPG",
     score: 8.3,
+    scorePopularite: 0,
 
     afficher() {
-        console.log(`Le jeu ${jeu2.titre}, ${jeu2.genre} sorti en ${jeu2.annee_de_sortie}, a un score de ${jeu2.score}.`);
+        console.log(`Le jeu ${jeu2.titre}, ${jeu2.genre} sorti en ${jeu2.annee_de_sortie}, a un score général de ${jeu2.score}.`);
     },
     getPopularite() {
         this.scorePopularite = (this.score*this.annee_de_sortie/100);
         return this.scorePopularite;
     }
 }
-jeu2.afficher();
-console.log(`Le score de popularité de ${jeu2.titre} est : ${jeu2.getPopularite()}`);
-// Voir comment comparer les deux scores et afficher les informations de celui qui a le score le plus élevé
+console.log(`Le score de popularité de ${jeu2.titre} est : ${jeu2.getPopularite().toFixed(2)}`);
+// console.log(jeu1.scorePopularite)
+if (jeu1.getPopularite()>jeu2.getPopularite()) {
+    console.log(`${jeu1.titre} est le jeu le plus populaire avec un score de : ${jeu1.getPopularite().toFixed(2)}`);
+    jeu1.afficher();
+} else {
+    console.log(`${jeu2.titre} est le jeu le plus populaire avec un score de : ${jeu2.getPopularite().toFixed(2)}`);
+    jeu2.afficher();
+}
